4f133fb16e770989950611ad6e3fb1e577a286ce
Boris Lokotkov <woweuro2010@gmail.com>
2023-04-02 18:02:30 +0300

    Change path of .env

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import { Server } from "socket.io";
 import * as dotenv from "dotenv";
 import { createServer } from "https";
-dotenv.config({ path: "../.env" });
+dotenv.config();
 import { env } from "./env";
 import gamesHandle from "./games";
 import roomsHandle, { getUniqueName } from "./rooms";
